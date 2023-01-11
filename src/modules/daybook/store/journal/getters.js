d4ba2e3a12a23a9375255ef1bd2f1b626bd98651
Juanjo Ramirez <juanjoramirezps@gmail.com>
2023-01-11 14:40:50 -0800

    working on

---
+++ src/modules/daybook/store/journal/getters.js
@@ -0,0 +1,6 @@
+// export const myGetter = (state) => {
+//     state.something
+// }
+
+export const getEntriesByTerm = (/*state*/) => {};
+export const getEntriesById = (/*state*/) => {};
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Officia ea sit officia ullamco est nulla cupidatat pariatur consectetur cupidatat. Et do incididunt enim ullamco. Velit quis voluptate ut ipsum eiusmod ex amet exercitation ea. Ex exercitation in ex et dolore minim. Cupidatat aliqua consectetur non qui reprehenderit anim veniam est ex officia nisi eu esse. Voluptate velit id sit consequat labore in commodo mollit in anim ut est. Anim aliquip ex consequat nostrud labore ad elit magna consequat.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime()+1000,
+            date: new Date().toDateString(),
+            text: 'Sunt deserunt nostrud dolore irure deserunt aute culpa ad. Non magna consequat reprehenderit aliqua ipsum. Tempor velit deserunt ut enim. Mollit dolore amet culpa aliquip occaecat cillum anim irure. Ad deserunt esse veniam exercitation veniam consequat tempor esse aliqua anim et anim id.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime()+2000,
+            date: new Date().toDateString(),
+            text: 'Dolore elit proident eiusmod consequat fugiat irure. Tempor et labore magna in eu. Magna exercitation aute ex voluptate consectetur qui pariatur et labore eiusmod. Elit nisi magna sint dolore adipisicing ea commodo ut. Non officia occaecat id Lorem anim. Nisi consequat ea reprehenderit est eiusmod tempor ipsum fugiat mollit pariatur minim nulla excepteur in.',
+            picture: null,
+        },
+    ],
+});
